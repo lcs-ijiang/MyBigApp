6f51894fdbab4a822605ba29ba56fcdb958f9c4d
jiangyishan <Yjiang@lcs.on.ca>
2026-03-02 10:58:30 -0500

    Initial Commit

---
+++ MyBigApp/MyBigAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBigAppApp.swift
+//  MyBigApp
+//
+//  Created by Student on 2/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct MyBigAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
