169f3f5654e5ad3275ce93654a74cfcb29153783
jiangyishan <Yjiang@lcs.on.ca>
2026-03-03 14:40:40 -0500

    Finish constructing tittle section of day 2 project interface

---
+++ Views/CampusConnectView.swift
@@ -0,0 +1,55 @@
+//
+//  CampusConnectView.swift
+//  MyBigApp
+//
+//  Created by Student on 3/3/2026.
+//
+
+import SwiftUI
+
+struct CampusConnectView: View {
+    @State private var selectedTab = 0
+    //0 for contacts 1 for discovery
+    
+    var body: some View {
+        NavigationStack {
+            VStack(spacing:0) {
+    //biulding heading
+        HStack{
+        Image(systemName: "person.2.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 50, height: 50)
+                .border(Color(.black), width: 1)
+                .padding(20)
+            VStack(alignment: .leading) {
+                Text("Campus")
+                Text("Connect")
+            }
+            .font(.title3.bold())
+            .foregroundColor(.primary)
+            
+           
+         
+        Spacer()
+            
+        Image(systemName: "gearshape")
+                .font(Font.largeTitle.bold())
+                .padding()
+                    
+                }
+        .padding()
+        .background(Color(.systemGray6))
+        .border(Color(.systemGray4), width: 1)
+         Spacer()
+                
+            }
+        
+            
+        }
+    }
+}
+
+#Preview {
+    CampusConnectView()
+}
